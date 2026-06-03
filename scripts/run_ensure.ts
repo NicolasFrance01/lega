541918234438d37537fcbf99e58c2523138d52aa
NicolasFrance01 <francenicolas.dw@gmail.com>
2026-06-03 08:31:13 -0300

    chore: remove scripts/run_ensure.ts that breaks build

--- scripts/run_ensure.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config({ path: '.env.local' });
-import { ensureIngresosExtColumns } from '../src/actions/ingresos';
-console.log("Database URL:", process.env.DATABASE_URL?.substring(0, 20) + "...");
-ensureIngresosExtColumns().then(console.log).catch(console.error);
